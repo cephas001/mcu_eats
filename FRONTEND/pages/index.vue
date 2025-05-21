81489a4bad11fe317fdad51530b68b461caf39c8
Peter Okodugha <okodughapeter58@gmail.com>
2025-05-21 10:56:19 +0000

    Add retailers and shops section. Turn all home sections to carousel view

--- FRONTEND/pages/index.vue
+++ FRONTEND/pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <section class="p-5" v-if="restaurants.length > 0">
+  <section
+    class="p-5"
+    v-if="restaurants.length > 0 || shops.length > 0 || retailers.length > 0"
+  >
     <UInput
       icon="i-material-symbols-search-rounded"
       size="lg"
@@ -15,36 +18,35 @@
   <section class="p-5" v-if="restaurants.length > 0">
     <h1 class="font-semibold mb-5 tracking-wide text-lg">Restaurants</h1>
 
-    <div
+    <UCarousel
+      v-slot="{ item }"
+      :items="restaurants"
+      dots
       class="rounded-md shadow-md text-sm mb-5 cursor-pointer hover:shadow-lg focus:shadow-lg"
-      v-for="restaurant in restaurants"
-      :key="restaurant.id"
     >
       <div class="relative">
         <div
           class="bg-primary w-fit py-1 px-2 rounded-r-full absolute top-10 left-[-4px] text-white text-sm z-100"
         >
-          {{ restaurant.category }}
+          {{ item.category }}
         </div>
         <Media
-          :src="restaurant.image"
+          :src="item.image"
           class="rounded-t-md"
-          @click.self="openRestaurant(restaurant.id, restaurant.open)"
+          @click.self="openRestaurant(item.id, item.open)"
         />
         <!-- icon to favorite would be to the top right -->
         <UIcon
           :name="`i-material-symbols-favorite${
-            restaurant.favourite ? '' : '-outline'
+            item.favourite ? '' : '-outline'
           }`"
           class="text-black absolute z-100 top-3 right-3 font-bold text-3xl"
-          :class="
-            restaurant.favourite ? 'animate-[var(--animate-pingOnce)]' : ''
-          "
-          @click.self="favouriteRestaurant(restaurant.id)"
+          :class="item.favourite ? 'animate-[var(--animate-pingOnce)]' : ''"
+          @click.self="favouriteRestaurant(item.id)"
         />
         <div
           class="bg-black/50 absolute inset-0 flex flex-col items-center justify-center gap-5 text-white text-lg"
-          v-if="!restaurant.open"
+          v-if="!item.open"
         >
           <p class="font-semibold">Closed</p>
 
@@ -54,7 +56,7 @@
 
       <div
         class="py-3 pl-5 flex justify-left items-center"
-        @click="openRestaurant(restaurant.id)"
+        @click="openRestaurant(item.id)"
       >
         <div>
           <!-- avatar -->
@@ -64,15 +66,139 @@
           />
         </div>
         <div class="ml-2 w-[70%]">
-          <h1 class="font-semibold tracking-wide">{{ restaurant.name }}</h1>
+          <h1 class="font-semibold tracking-wide">{{ item.name }}</h1>
           <p
             class="text-gray-700 text-sm whitespace-nowrap overflow-hidden text-ellipsis"
           >
-            {{ restaurant.description }}
+            {{ item.description }}
           </p>
         </div>
       </div>
-    </div>
+    </UCarousel>
+  </section>
+
+  <section class="p-5" v-if="shops.length > 0">
+    <h1 class="font-semibold mb-5 tracking-wide text-lg">Shops</h1>
+
+    <UCarousel
+      v-slot="{ item }"
+      :items="shops"
+      dots
+      class="rounded-md shadow-md text-sm mb-5 cursor-pointer hover:shadow-lg focus:shadow-lg"
+    >
+      <div class="relative">
+        <div
+          class="bg-primary w-fit py-1 px-2 rounded-r-full absolute top-10 left-[-4px] text-white text-sm z-100"
+        >
+          {{ item.category }}
+        </div>
+        <Media
+          :src="item.image"
+          class="rounded-t-md"
+          @click.self="openRestaurant(item.id, item.open)"
+        />
+        <!-- icon to favorite would be to the top right -->
+        <UIcon
+          :name="`i-material-symbols-favorite${
+            item.favourite ? '' : '-outline'
+          }`"
+          class="text-black absolute z-100 top-3 right-3 font-bold text-3xl"
+          :class="item.favourite ? 'animate-[var(--animate-pingOnce)]' : ''"
+          @click.self="favouriteRestaurant(item.id)"
+        />
+        <div
+          class="bg-black/50 absolute inset-0 flex flex-col items-center justify-center gap-5 text-white text-lg"
+          v-if="!item.open"
+        >
+          <p class="font-semibold">Closed</p>
+
+          <p>Opens at 7:00am</p>
+        </div>
+      </div>
+
+      <div
+        class="py-3 pl-5 flex justify-left items-center"
+        @click="openRestaurant(item.id)"
+      >
+        <div>
+          <!-- avatar -->
+          <img
+            src="@/assets/images/avatars/avatar.jpg"
+            class="w-[40px] rounded-full"
+          />
+        </div>
+        <div class="ml-2 w-[70%]">
+          <h1 class="font-semibold tracking-wide">{{ item.name }}</h1>
+          <p
+            class="text-gray-700 text-sm whitespace-nowrap overflow-hidden text-ellipsis"
+          >
+            {{ item.description }}
+          </p>
+        </div>
+      </div>
+    </UCarousel>
+  </section>
+
+  <section class="p-5" v-if="retailers.length > 0">
+    <h1 class="font-semibold mb-5 tracking-wide text-lg">Retailers</h1>
+
+    <UCarousel
+      v-slot="{ item }"
+      :items="retailers"
+      dots
+      class="rounded-md shadow-md text-sm mb-5 cursor-pointer hover:shadow-lg focus:shadow-lg"
+    >
+      <div class="relative">
+        <div
+          class="bg-primary w-fit py-1 px-2 rounded-r-full absolute top-10 left-[-4px] text-white text-sm z-100"
+        >
+          {{ item.category }}
+        </div>
+        <Media
+          :src="item.image"
+          class="rounded-t-md"
+          @click.self="openRestaurant(item.id, item.open)"
+        />
+        <!-- icon to favorite would be to the top right -->
+        <UIcon
+          :name="`i-material-symbols-favorite${
+            item.favourite ? '' : '-outline'
+          }`"
+          class="text-black absolute z-100 top-3 right-3 font-bold text-3xl"
+          :class="item.favourite ? 'animate-[var(--animate-pingOnce)]' : ''"
+          @click.self="favouriteRestaurant(item.id)"
+        />
+        <div
+          class="bg-black/50 absolute inset-0 flex flex-col items-center justify-center gap-5 text-white text-lg"
+          v-if="!item.open"
+        >
+          <p class="font-semibold">Closed</p>
+
+          <p>Opens at 7:00am</p>
+        </div>
+      </div>
+
+      <div
+        class="py-3 pl-5 flex justify-left items-center"
+        @click="openRestaurant(item.id)"
+      >
+        <div>
+          <!-- avatar -->
+          <img
+            src="@/assets/images/avatars/avatar.jpg"
+            class="w-[40px] rounded-full"
+          />
+        </div>
+        <div class="ml-2 w-[70%]">
+          <h1 class="font-semibold tracking-wide">{{ item.name }}</h1>
+          <p
+            class="text-gray-700 text-sm whitespace-nowrap overflow-hidden text-ellipsis"
+          >
+            {{ item.description }}
+          </p>
+        </div>
+      </div>
+    </UCarousel>
   </section>
 
   <section
@@ -86,10 +212,19 @@
 <script setup>
 import { onMounted } from "vue";
 const restaurants = ref([]);
+const shops = ref([]);
+const retailers = ref([]);
 
 const fetchRestaurants = async () => {
   restaurants.value = await $fetch("api/restaurants");
-  console.log(restaurants.value);
+};
+
+const fetchShops = async () => {
+  shops.value = await $fetch("api/shops");
+};
+
+const fetchRetailers = async () => {
+  retailers.value = await $fetch("api/retailers");
 };
 
 const favouriteRestaurant = async (restaurantId) => {
@@ -110,5 +245,7 @@ const openRestaurant = (id, open) => {
 
 onMounted(() => {
   fetchRestaurants();
+  fetchShops();
+  fetchRetailers();
 });
 </script>
