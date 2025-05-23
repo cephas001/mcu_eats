5623fc0cb4e5b383b1cf6315d5854fa5553b4ea0
Paulooodev <131523328+Paulooodev@users.noreply.github.com>
2025-05-23 13:23:23 +0000

    Profile, support,and favourites styling

--- FRONTEND/pages/favourites.vue
+++ FRONTEND/pages/favourites.vue
@@ -1,1 +1,59 @@
-<template></template>
+<template>
+    <section class="min-h-screen">
+        <header class="favourites-header px-6 z-1000 bg-white sticky top-0 text-md shadow-md">
+            <div class="flex px-4 py-3 items-center w-full">
+                <div >
+                    <UIcon
+                    name="i-material-symbols-light-close"
+                    class="text-red-500 w-8 h-8 mr-4 mt-2"
+                    />
+                </div>
+                <div class="flex justify-center font-manrope">
+                    <h1 class="text-xl font-bold text-gray-800">Favorites</h1>
+                </div>
+                
+            </div>
+            <div class="flex items-center justify-center space-x-5">
+                <button 
+                    v-for="tab in tabs"
+                    :key="tab"
+                    class="pb-3 px-1 text-sm font-light relative"
+                    :class="{
+                        'text-black': activeTab !== tab,
+                        'text-black': activeTab === tab,
+                    }"
+                    @click="activeTab = tab"
+                >
+                {{  tab }}
+                
+                <span
+                    v-if="activeTab === tab"
+                    class="absolute bottom-0 top-8 left-0 right-0 h-0.5 bg-primary animate-underline"
+                />
+                </button>
+            </div>
+        </header>
+
+        <div class="container relative min-h-[calc(100vh-100px)]">
+            <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <div class="flex flex-col items-center justify-center">
+                    <UIcon
+                        name="i-material-symbols-light-favorite"
+                        class="text-gray-400 w-25 h-25"
+                    />
+                    <p class="text-center whitespace-nowrap">You have no favourites nearby</p>
+                    <div class="btn-container mt-5">
+                        <button class="px-6 py-3 rounded-full cursor-pointer text-white bg-primary text-lg">Find Food</button>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+       
+    </section>
+</template>
+
+<script setup>
+    const tabs = ['Vendors','Menu Items'];
+    const activeTab = ref('Menu Items');
+</script>
--- FRONTEND/pages/support.vue
+++ FRONTEND/pages/support.vue
@@ -1,1 +1,127 @@
-<template></template>
+<template>
+    <section class="min-h-screen">
+        <header class="favourites-header px-6 py-3 z-1000 bg-white sticky top-0 text-md mb-5">
+                <div class="flex px-4 py-3 items-center w-full">
+                    <UIcon
+                    name="i-material-symbols-light-close"
+                    class="text-red-600 w-8 h-8 mr-5"
+                    />
+                    <div >
+                        <div class="flex-grow text-center font-manrope">
+                            <h1 class="text-xl font-bold text-gray-800">Contact support</h1>
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+            <div class="flex flex-col items-center justify-center">
+                <div class="mt-3">
+                    <Media src="/support.jpg" class="w-40 h-auto" />
+                </div>
+                <p class=" text-black text-bold text-center">Let us know your issue by selecting any of your preferred choice of communication below</p>
+            </div>
+
+            <!-- Name -->
+           <div
+            class="flex items-center justify-between py-2 px-6 border-b border-gray-200 bg-white font-manrope text-md mt-3"
+            >
+                <div class="flex items-center">
+                <UIcon
+                    name="i-material-symbols-light-mail-outline"
+                    class="text-primary text-xl w-7 h-7 mr-4 self-center"
+                />
+                 <div class="flex flex-col justify-center">
+                    <span class="text-black font-bold">Email Us</span>
+                    <p class="text-gray-600 text-sm">We go reply when you press us 2k</p>
+                </div>
+                
+                </div>
+
+                <div
+                class="flex items-center p-1 cursor-pointer"
+                >
+                <UIcon
+                    name="i-material-symbols-arrow-right"
+                    class="text-gray-600 text-xl w-7 h-7"
+                />
+                </div>
+            </div>
+            <!-- WhatsApp -->
+            <div
+            class="flex items-center justify-between py-2 px-6 border-b border-gray-200 bg-white font-manrope text-md mt-3"
+            >
+                <div class="flex items-center">
+                <UIcon whatsapp
+                    name="i-material-symbols-whatsapp"
+                    class="text-primary text-xl w-7 h-7 mr-4"
+                 />
+                 <div class="flex flex-col justify-center">
+                    <span class=" font-bold text-black">Chat on WhatsApp</span>
+                    <p class="text-gray-600 text-sm">Reply Asap</p>
+                </div>
+                </div>
+                <UIcon
+                    name="i-material-symbols-arrow-right"
+                    class="text-gray-600 text-xl w-7 h-7"
+                />
+            </div>
+            <!-- Username -->
+            <div
+                class="flex items-center justify-between py-2 px-6 border-b border-gray-200 bg-white font-manrope text-md mt-3"
+            >
+                <div class="flex items-center">
+                <UIcon
+                    name="i-material-symbols-call"
+                    class="text-primary text-xl w-7 h-7 mr-4"
+                />
+                <div class="flex flex-col justify-center">
+                   <span class=" font-bold text-black">Call Us</span>
+                    <p class="text-gray-600 text-sm">Business hours everyday</p>
+                </div>
+                </div>
+                <UIcon
+                    name="i-material-symbols-arrow-right"
+                    class="text-gray-600 text-xl w-7 h-7"
+                />
+               
+            </div>
+            
+            <!-- Order Problem -->
+            <div
+            class="flex items-center justify-between py-2 px-6 border-b border-gray-200 bg-white font-manrope text-md mt-3"
+            >
+                <div class="flex items-center">
+                 <UIcon name="i-material-symbols-contact-support-outline" class="text-primary w-7 h-7 mr-4" />
+                  <div class="flex flex-col justify-center">
+                    <span class=" font-bold text-black">Help with an order</span>
+                    <p class="text-gray-600 text-sm">Problem with your order?</p>
+                </div>   
+                
+                </div>
+                <UIcon
+                    name="i-material-symbols-arrow-right"
+                    class="text-gray-600 text-xl w-7 h-7"
+                />
+                
+            </div>
+            <!-- Live Chat -->
+            <div
+            class="flex items-center justify-between py-2 px-6 border-b border-gray-200 bg-white font-manrope text-md mt-3"
+            >
+                <div class="flex items-center">
+                <UIcon
+                    name="i-material-symbols-support-agent"
+                    class="text-primary w-7 h-7 text-xl mr-4"
+                />
+                <div class="flex flex-col justify-center">
+                    <span class=" font-bold text-black">Live Chat</span>
+                    <p class="text-gray-600 text-sm">Chat live with our in-app agents</p>
+                </div>
+                </div>
+                <UIcon
+                    name="i-material-symbols-arrow-right"
+                    class="text-gray-600 text-xl w-7 h-7"
+                />
+            </div>
+    </section>
+</template>
